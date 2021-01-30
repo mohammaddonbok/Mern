1a04a99031c77284947b0cfb354bb365011ffd72
Mohammad Donbok <mdonbok@gmail.com>
2021-01-30 11:36:01 +0200

    first component usage

---
+++ React/first-react-jsx/src/components/Hello.js
@@ -0,0 +1,18 @@
+
+import React,{Component} from 'react' ;
+
+class Hello extends Component{
+    render(){
+        return(<>
+            <h1>Hello Dojo!</h1>
+
+            <ul>Things i need to do:
+                <li>Learn React</li>
+                <li>Climbe Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </>);
+    }
+}
+export default Hello;
