5b3719fd50cca4ea4821e26a0f54f8d4eda89200
Mohammad Donbok <mdonbok@gmail.com>
2021-01-30 15:02:54 +0200

    updated

--- React/prop-it-up/src/components/PersonCard .js
+++ React/prop-it-up/src/components/PersonCard .js
@@ -2,14 +2,28 @@ import React,{ Component } from "react";
 
 
 class PersonalCard extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            age:this.props.age
+        }
+    }
+    incrementOne(){
+        this.setState({
+            age:this.state.age+1
+        })
+    }
     render(){
-        const { firstName, lastName ,age ,hairColor } = this.props;
+    
+        const { firstName, lastName ,hairColor } = this.props;
+        const{age}=this.state;
         return (
             <>
             <div>
                 <h1>{lastName},{firstName}</h1>
                 <p>Age:{age}</p>
                 <p>Hair Color:{hairColor}</p>
+                <button onClick={()=>this.incrementOne()}>Birthday Button for {firstName} {lastName} </button>
             </div>
             </>
             );
