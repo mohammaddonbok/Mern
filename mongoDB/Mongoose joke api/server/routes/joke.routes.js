1d6bba3a0010fd5fa4f6681fa56216cf5ceb3ba0
Mohammad Donbok <mdonbok@gmail.com>
2021-02-10 17:23:28 +0200

    Mongo DB + Mongoose

---
+++ mongoDB/Mongoose joke api/server/routes/joke.routes.js
@@ -0,0 +1,11 @@
+const JokeController = require("../controllers/joke.controller");
+
+module.exports = app =>{
+    app.get("/api/jokes/",JokeController.findAllJokes);
+    app.get("/api/jokes/:id",JokeController.findOneSingleJoke);
+    app.get("/api/jokes/random/random",JokeController.randomJoke);
+    app.post("/api/jokes/new/",JokeController.createNewJoke);
+    app.put("/api/jokes/update/:id",JokeController.updateExistingJoke);
+    app.delete("api/jokes/delete/:id",JokeController.deleteAnExistingJoke);
+
+};
